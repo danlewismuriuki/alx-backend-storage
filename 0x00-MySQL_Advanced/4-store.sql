d61c9a4b927c0fddc5a24fb092e4e9875b1b9acf
Dan <danlewismuriuki@gmail.com>
2024-08-09 00:51:25 +0300

    Create `items` and `orders` tables with trigger to manage inventory
    - Added `items` table to track stock quantity with columns `name` and `quantity`.
    - Added `orders` table to record orders with columns `item_name` and `number`.
    - Implemented a trigger (`decreases_quantity_after_order`) that automatically decreases the quantity of an item in the `items` table after a new order is inserted into the `orders` table.
    - Ensured data consistency and automated inventory updates.

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,17 @@
+--  script that creates a trigger that decreases the quantity
+-- of an item after adding a new order.
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS decreases_quantity_after_order$$
+
+CREATE TRIGGER decreases_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END$$
+
+DELIMITER ;
