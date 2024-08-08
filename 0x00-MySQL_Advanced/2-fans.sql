f8930b1c209ab8bf6740db1fc5d4e4cdbbd86d3a
Dan <danlewismuriuki@gmail.com>
2024-08-08 15:53:20 +0300

    Fix SQL query: Update table name from 'meta_bands' to 'metal_bands'
    Revised the SQL script to use the correct table name 'metal_bands' instead of 'meta_bands' to resolve the table not found error. Updated the query to correctly rank country origins of bands by the number of (non-unique) fans.

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- SQL Script ranks country origins of bands
+-- ordered by the number of (non-unique) fans
+
+SELECT origin, SUM(fans) As nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
