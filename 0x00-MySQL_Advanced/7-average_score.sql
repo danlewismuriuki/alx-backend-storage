e73f95bb5c3a9e34b706edb6ab937914d4481621
Dan <danlewismuriuki@gmail.com>
2024-08-10 00:42:11 +0300

    Add stored procedure `AddBonus` to handle new corrections
    - Created a stored procedure `AddBonus` that:
      - Accepts `user_id`, `project_name`, and `score` as input parameters.
      - Checks if the `project_name` exists in the `projects` table.
      - Inserts a new project if the name does not exist.
      - Inserts a new correction entry into the `corrections` table.
    
    - Corrected syntax in the procedure for inserting into `corrections` table.

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,28 +1,12 @@
--- SQL script that creates stored procedure ComputeAverageScoreForUser
--- computes and store the average score for a student.
--- An average score can be a decimal
-
--- Drop the existing procedure
+--  creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-
 DELIMITER $$
--- create the procedure with 1 arg
 CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT)
-BEGIN 
-    -- declare variable to stroee temp avg score
+BEGIN
     DECLARE avg_score FLOAT;
-
-    -- Calculate the avg score for the corrections Table for score column
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Updat  the users Table and set the average score
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
-
-END $$
-
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;
